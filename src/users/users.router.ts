fdf872eaae2eb937758db05fddaa0ba2b2d1377d
Almog Vagman Ciprut <almogvc@gmail.com>
2019-01-20 15:30:54 +0200

    refactor: Remove users router

--- src/users/users.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { UsersController } from './users.controller';
-import { Wrapper } from '../utils/wrapper';
-
-const UsersRouter: Router = Router();
-
-UsersRouter.get('/many', Wrapper.wrapAsync(UsersController.get));
-
-export { UsersRouter };
