0d1e389e9129a2cdc478bbab96a04d643ee65050
Ron Borysovski <roni537@gmail.com>
2019-01-21 16:51:56 +0200

    refactor: Fetch channels views on get many channels

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from 'express';
 import { ChannelsService } from './channels.service';
+import { VideosRpc } from '../videos/videos.rpc';
 
 export class ChannelsController {
     static async getMany(req: Request, res: Response) {
-        const channels = await ChannelsService.getMany(req.query, req.headers.authorization!);
-        // Add views
-        res.json(channels);
+        let channels = await ChannelsService.getMany(req.query, req.headers.authorization!);
+        const channelIds = channels.map((channel: { id: string }) => channel.id);
+
+        try {
+            const views = await VideosRpc.getChannelsViews(channelIds);
+
+            channels = channels.map((channel: any) => {
+                channel.views = views[channel.id];
+
+                return channel;
+            });
+        } finally {
+            return res.json(channels);
+        }
+
     }
 }
