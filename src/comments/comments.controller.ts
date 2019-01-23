4f4289135f43b1d347d0871e230b56d4153abefe
Ron Borysovski <roni537@gmail.com>
2019-01-23 13:04:02 +0200

    refactor: Adjust compositions

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,56 +1,50 @@
 import { Request, Response } from 'express';
+import { ReactionsRpc } from '../reactions/reactions.rpc';
+import { UsersRpc } from '../users/users.rpc';
 import { VideosService } from '../videos/videos.service';
 import { CommentsService } from './comments.service';
-import { UsersRpc } from '../users/users.rpc';
-import { ReactionsRpc } from '../reactions/reactions.rpc';
 
 export class CommentsController {
     static async getRootComments(req: Request, res: Response) {
-        let comments = await CommentsService.getRoot(req.query, req.headers.authorization!);
+        const comments = await CommentsService.getRoot(req.query, req.headers.authorization!);
 
-        const usersIds = comments.map((comment: any) => comment.user);
-        const reactionsResources = comments.map((comment: any) => comment.id);
-
-        try {
-            const users = await UsersRpc.getUsersByIds(usersIds);
-            const reactions = await ReactionsRpc.getReactionsByResources(reactionsResources);
-
-            comments = comments.map((comment: any) => {
-                return {
-                    ...comment,
-                    user: users[comment.user],
-                    reactions: reactions[comment.id],
-                };
-            });
-        } finally {
-            return res.json(comments);
-        }
+        res.json(await CommentsController.expandCommentsInformation(comments));
     }
 
     static async getReplies(req: Request, res: Response) {
-        let comments = await CommentsService.getReplies(req.params.parent, req.headers.authorization!);
+        const comments = await CommentsService.getReplies(req.params.parent, req.headers.authorization!);
 
+        res.json(await CommentsController.expandCommentsInformation(comments));
+    }
+
+    private static async expandCommentsInformation(comments: any) {
+        let expandedComments;
         const usersIds = comments.map((comment: any) => comment.user);
         const reactionsResources = comments.map((comment: any) => comment.id);
 
         try {
             const users = await UsersRpc.getUsersByIds(usersIds);
             const reactions = await ReactionsRpc.getReactionsByResources(reactionsResources);
+            const reactionsMap: { [resource: string]: Object } = {};
 
-            comments = comments.map((comment: any) => {
+            reactions.forEach((reaction: any) => {
+                reactionsMap[reaction.resource] = reaction.types;
+            });
+
+            expandedComments = comments.map((comment: any) => {
                 return {
                     ...comment,
                     user: users[comment.user],
-                    reactions: reactions[comment.id],
+                    reactions: reactionsMap[comment.id],
                 };
             });
         } finally {
-            return res.json(comments);
+            return expandedComments;
         }
     }
 
     static async create(req: Request, res: Response) {
-        await VideosService.doesExist(req.body.resource);
-        res.json(await CommentsService.create(req.body));
+        await VideosService.doesExist(req.body.resource, req.headers.authorization!);
+        res.json(await CommentsService.create(req.body, req.headers.authorization!));
     }
 }
