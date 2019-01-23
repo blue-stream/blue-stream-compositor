e71337da0b9eafc62b0273b05ac874bef2b62050
Ron Borysovski <roni537@gmail.com>
2019-01-23 13:04:39 +0200

    style: Change param name

--- src/users/users.rpc.ts
+++ src/users/users.rpc.ts
@@ -5,8 +5,8 @@ const jayson = require('jayson/promise');
 export class UsersRpc {
     private static rpcClient = jayson.Client.http(`${config.endpoints.users.hostname}:${config.endpoints.users.rpc.port}`);
 
-    static async getUsersByIds(resources: string[]) {
-        const response = await UsersRpc.rpcClient.request(config.endpoints.users.rpc.methods.GET_USERS_BY_IDS, resources);
+    static async getUsersByIds(ids: string[]) {
+        const response = await UsersRpc.rpcClient.request(config.endpoints.users.rpc.methods.GET_USERS_BY_IDS, ids);
 
         return response.result;
     }
