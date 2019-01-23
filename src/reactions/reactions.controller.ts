6c40ea6e87d21428cd8f43fd5ea1a6defb65a5e3
Ron Borysovski <roni537@gmail.com>
2019-01-23 13:04:26 +0200

    refactor: Pass request token header

--- src/reactions/reactions.controller.ts
+++ src/reactions/reactions.controller.ts
@@ -6,11 +6,11 @@ import { ReactionsService } from './reactions.service';
 export class ReactionsController {
     static async create(req: Request, res: Response) {
         if (req.body.resourceType === 'COMMENT') {
-            await CommentsService.doesExist(req.body.resource);
+            await CommentsService.doesExist(req.body.resource, req.headers.authorization!);
         } else if (req.body.resourceType === 'VIDEO') {
-            await VideosService.doesExist(req.body.resource);
+            await VideosService.doesExist(req.body.resource, req.headers.authorization!);
         }
 
-        res.json(await ReactionsService.create(req.body));
+        res.json(await ReactionsService.create(req.body, req.headers.authorization!));
     }
 }
